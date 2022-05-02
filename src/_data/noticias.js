ac84074f4ba0f1f2057c2de0094a481f6906f301
pslasso <paul.lasso.91@gmail.com>
2022-05-02 20:28:49 +0200

    CAmbio API para titulares

--- src/_data/noticias.js
+++ src/_data/noticias.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function () {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}&pageSize=6&language=es`);
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
         return response.data;
         console.log(response);
     } catch (error) {
