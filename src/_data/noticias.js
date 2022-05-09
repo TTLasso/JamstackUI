144ddc2003796fc110f5dbcc30ef171a9044e8dd
pslasso <paul.lasso.91@gmail.com>
2022-05-09 12:04:38 +0200

    env variables en Netlify

--- src/_data/noticias.js
+++ src/_data/noticias.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 require('dotenv').config();
 
+
 module.exports = async function () {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=ar&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
         return response.data;
         console.log(response);
     } catch (error) {
