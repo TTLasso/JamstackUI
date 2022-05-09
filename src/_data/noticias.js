c32abb687df1de29219d027157bc2f8fe814069b
pslasso <paul.lasso.91@gmail.com>
2022-05-09 11:47:41 +0200

    API clima con interaccion de usuario

--- src/_data/noticias.js
+++ src/_data/noticias.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function () {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=ef7de1b0f997489195cd40fe164ce3f8&pageSize=6`);
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
         return response.data;
         console.log(response);
     } catch (error) {
---
+++ src/js/weather.js
@@ -0,0 +1,23 @@
+
+document.getElementById('generate').addEventListener('click', success);
+
+function success(){
+    const input = document.getElementById('input').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=13862fd0b0c3a6fa22692ba012e4163f`;
+    fetch(url)
+    .then((response) => {
+    return response.json();
+})
+.then((data) => {
+
+    document.getElementById('city').innerHTML=data.name;
+    document.getElementById('temp').innerHTML=data.main.temp;
+    document.getElementById('main').innerHTML=data.weather[0].main;
+    document.getElementById('desc').innerHTML=data.weather[0].description;
+
+    console.log('data:',data);
+
+});
+
+}
+
