60ab7c8280b037f84bb226f695b643ee4fba318d
pslasso <paul.lasso.91@gmail.com>
2022-05-09 12:08:03 +0200

    Arreglos variables de entorno

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,9 +1,11 @@
 
+
+
 document.getElementById('generate').addEventListener('click', success);
 
 function success(){
     const input = document.getElementById('input').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${process.env.WEATHER_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=13862fd0b0c3a6fa22692ba012e4163f`;
     fetch(url)
     .then((response) => {
     return response.json();
