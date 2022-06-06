f95898baf7a25a55d56350fbc752e3afe8a8370e
pslasso <paul.lasso.91@gmail.com>
2022-06-06 12:03:58 +0200

    Plugin Images de Eleventy instalado

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,32 @@
+const Image = require('@11ty/eleventy-img');
+
+async function imageShortcode(src, alt, sizes) {
+    let metadata = await Image(src, {
+        widths: [null],
+        formats: ["jpeg"],
+        urlPath: "/images/",
+        outputDir: "./dist/images/",
+    });
+
+    let imageAttributes = {
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes, {
+        whitespaceMode: "inline"
+    });
+}
+
+
 module.exports = function (config) {
     config.addPassthroughCopy('src/css');
     config.addPassthroughCopy('src/images');
     config.addWatchTarget('src/css');
     config.addPassthroughCopy("src/js");
+    config.addNunjucksAsyncShortcode("image", imageShortcode);
     return {
         dir: {
             input: "src",
@@ -11,3 +35,4 @@ module.exports = function (config) {
         }
     };
 };
+
