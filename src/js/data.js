cdb6212d22a3f580c5525d3062644f386493b840
pslasso <paul.lasso.91@gmail.com>
2022-06-10 10:50:15 +0200

    Eliminar entradas del diario, mejoras de diseño

--- src/js/data.js
+++ src/js/data.js
@@ -21,8 +21,49 @@ document.getElementById('getNews').addEventListener('click', async function getD
     }).then((response) => response.json());
     console.log({ result })
     for (let i = 0; i < result.data.entradas_diario.length; i++) {
+        let ingreso = document.createElement("div");
         let entrada = document.createElement("div");
-        document.getElementById('faunaData').appendChild(entrada);
-        entrada.innerHTML = `${result.data.entradas_diario[i].ciudad} ${result.data.entradas_diario[i].entrada} ${result.data.entradas_diario[i].temperatura}`
+        let borrar = document.createElement("button");
+        ingreso.appendChild(entrada);
+        ingreso.appendChild(borrar);
+        borrar.innerText = `Eliminar entrada`;
+        document.getElementById('faunaData').appendChild(ingreso);
+        let id = result.data.entradas_diario[i].id;
+        entrada.innerHTML = `Entrada en: ${result.data.entradas_diario[i].ciudad}-- Texto ingresado: ${result.data.entradas_diario[i].entrada}-- La temperatura fue: ${result.data.entradas_diario[i].temperatura}°C`
+        ingreso.setAttribute("id", `parent${id}`);
+        borrar.setAttribute("id", id);
+        async function deleteData() {
+            console.log(`mostrando ${id}`);
+            const deleteData = await fetch('https://decent-malamute-79.hasura.app/v1/graphql', {
+                method: 'POST',
+                HEADERS: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    query: `
+                    mutation MyMutation {
+                        delete_entradas_diario_by_pk(id: "${id}") {
+                          id
+                        }
+                      }
+                      
+              
+        `,
+                    variables: {
+                    }
+                })
+            })
+            document.getElementById(`parent${id}`).setAttribute("style", "display: none");
+        }
+        borrar.addEventListener('click', function confirm() {
+            var resultado = window.confirm('¿Deseas eliminar la entrada?');
+            if (resultado === true) {
+                deleteData();
+                window.alert('Entrada borrada');
+            } else {
+                window.alert('Entrada conservada');
+            }
+        })
+
     }
 });
--- src/js/weather.js
+++ src/js/weather.js
@@ -2,7 +2,6 @@
 document.getElementById('generate').addEventListener('click', success);
 
 
-
 function success() {
     const input = document.getElementById('input').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=13862fd0b0c3a6fa22692ba012e4163f&units=metric&lang=sp`;
