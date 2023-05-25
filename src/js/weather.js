a0d3c452fdc85bfa9e6ecffa22c82631524e29da
Paul Lasso <paul.lasso.91@gmail.com>
2023-05-25 15:30:22 +0200

    Update weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -4,7 +4,7 @@ document.getElementById('generate').addEventListener('click', success);
 
 function success() {
     const input = document.getElementById('input').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=13862fd0b0c3a6fa22692ba012e4163f&units=metric&lang=sp`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${API_KEY}&units=metric&lang=sp`;
     fetch(url)
         .then((response) => {
             return response.json();
